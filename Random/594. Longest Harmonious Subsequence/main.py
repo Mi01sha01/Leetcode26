d457f32be44f73745ab63e43d1a7c373df1fc7aa
Mi01sha01 <midegaurgesa@gmail.com>
2026-07-05 07:12:52 +0300

    Create 594. Longest Harmonious Subsequence/main.py
    # Count the frequency of each number, then check each number x to see
    # if x + 1 exists and update the longest harmonious subsequence length.

---
+++ Random/594. Longest Harmonious Subsequence/main.py
@@ -0,0 +1,12 @@
+from collections import Counter
+
+class Solution:
+    def findLHS(self, nums: List[int]) -> int:
+        count = Counter(nums)
+        ans = 0
+
+        for x in count:
+            if x + 1 in count:
+                ans = max(ans, count[x] + count[x + 1])
+
+        return ans
